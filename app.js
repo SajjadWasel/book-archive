c2bd1b3f073ced66e11a6c2e9fa5e2a674764f7f
SajjadWasel <sajjad0123wasel@gmail.com>
2021-09-03 22:50:34 +0600

    third commit

--- app.js
+++ app.js
@@ -4,30 +4,47 @@ const searchBook = () => {
     const searchText = searchArea.value;
     searchArea.value = "";
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    const resultContainer = document.getElementById("result-container");
+    resultContainer.innerHTML = ``;
+
     /* ----fetching the url---- */
     fetch(url)
         .then(res => res.json())
-        .then(data => displayResults(data.docs))
+        .then(data => displayResults(data.docs, data.numFound));
+
 }
 
-const displayResults = books => {
+
+const displayResults = (books, total) => {
+    // ------------total result-----------------
+    const totalResult = document.getElementById("total-result");
+    totalResult.innerText = `Total Result: ${total}`;
+    //---------------books---------------------
     const resultContainer = document.getElementById("result-container");
     books.forEach(book => {
-        console.log(book);
         const div = document.createElement("div");
         div.classList.add("col-md-4");
+
         div.innerHTML = `
-        <div class="card m-1 h-75 w-75">
+        <div class="card h-100 w-100">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
             class="card-img-top h-50" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
-                <small class="text-secondary">Last Publish: 2021</small>
+            <div class="card-body h-25">
+
+                <h5 class="card-title">
+                ${book.title}
+                </h5>
+                <p class="card-text">
+                Author: ${book.author_name[0]}</p>
+                <p>Publisher: ${book.publisher[0]}</p>
+                
+            </div>
+            <div class="card-footer">
+                    <small class="text-muted">First Publish: ${book.first_publish_year}</small>
             </div>
         </div>
         `
         resultContainer.appendChild(div);
     });
 }
+
